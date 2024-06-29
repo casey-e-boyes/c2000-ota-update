fb24daadf54fba0dac894b42c669f29c4924d59f
casey-e-boyes <casey-e-boyes@proton.me>
2024-06-29 21:11:44 +1200

    Added assert checking of parameterised test for the serial rx crc; struct address had to be hardcoded however

--- test/pytest/test-serial.py
+++ test/pytest/test-serial.py
@@ -1,6 +1,18 @@
-import pytest, time
+import pytest, time, zlib
 from DSSClient import DSSClient
 from serial import Serial
+
+test_messages = [
+    ("message from pytest!", zlib.crc32(b'message from pytest!') & 0xFFFFFFFF),
+    ("MESSAGE FROM PYTEST!", zlib.crc32(b'MESSAGE FROM PYTEST!') & 0xFFFFFFFF),
+    ("Message from pytest!", zlib.crc32(b'Message from pytest!') & 0xFFFFFFFF)
+]
+
+print("\n")
+print("[TEST MESSAGES & EXPECTED CRC CODES]")
+for message in test_messages:
+    print("    {}: 0x{:08X}".format(message[0], message[1]))
+print("")
 
 
 @pytest.fixture
@@ -11,8 +23,8 @@ def debugger():
 def tty():
     return Serial('/dev/ttyACM0', 9600, timeout = 1)
 
-@pytest.mark.parametrize("message", ["message from pytest!", "MESSAGE FROM PYTEST!", "Message from pytest!"])
-def test_serial_rx(debugger, tty, message: str):
+@pytest.mark.parametrize("message, expected_crc", test_messages)
+def test_serial_rx(debugger, tty, message: str, expected_crc: int):
     debugger.__enter__()
     debugger.command({"name":"load" , "program":"/home/runner/projects/c2000-ota-update/build/firmware.elf"})
     debugger.command({"name": "runAsynch"})
@@ -33,12 +45,12 @@ def test_serial_rx(debugger, tty, message: str):
         "signed": 0,
     }, True)
 
-    rx_crc_address = (debugger.command({"name": "getVariableAddress" , "variable":"serial_rx_buf"}, True))
-    print("{}:{}".format(type(rx_crc_address), rx_crc_address))
+    # rx_crc_address = (debugger.command({"name": "getVariableAddress" , "variable":"serial_rx_buf"}, True))
+    # print("{}:{}".format(type(rx_crc_address), rx_crc_address))
     rx_crc_value = debugger.command({
         "name": "readData",
-        "page": 1,
-        "address": int(rx_crc_address) + 5,
+        "page": 0,
+        "address": 0x0000AADE + 6, # Hard-coded struct member address from firmware.map
         "typeSize": 32,
         "signed": 0,
     }, True)
@@ -52,8 +64,10 @@ def test_serial_rx(debugger, tty, message: str):
         rx_message += (str(chr(int( '{:08b}'.format((int(raw_char) & 0xFF))[::-1], 2  ))))
         rx_message += (str(chr(int( '{:08b}'.format(((int(raw_char) >> 8) & 0xFF))[::-1], 2  ))))
 
-    print("\x1b[1;34m{:>{length}}\x1b[0m".format(rx_message, length = len(rx_message) + 25))
+    print("                MESSAGE: \x1b[1;34m{}\x1b[0m".format(rx_message))
+    print("                    CRC: \x1b[1;33m0x{:08X}\x1b[0m".format(int(rx_crc_value)))
 
-    print((rx_crc_value))
+    assert int(rx_crc_value) == expected_crc
+    assert rx_message == message
 
     debugger.__exit__()
